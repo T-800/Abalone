33032c2fb94ea792ffeb10d3eeca9d3f303f17bf
T-800 <adequin.renaud@gmail.com>
2013-05-05 10:57:02 +0200

    s

---
+++ Projet/fichier.h
@@ -0,0 +1,16 @@
+#ifndef FICHIER_H_INCLUDED
+#define FICHIER_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "plateau.h"
+
+/*
+Cette fonction initialise le plateau.
+elle est appelé avec l'otopn -c donnée au programme 
+*/
+int fichierConfig(const char* fic, Plateau *plateau);
+
+int sauvegarde(char* fic,Plateau *plateau);
+
+#endif
