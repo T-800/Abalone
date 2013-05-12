f8aa7a58443947faafded0d702b80422898be142
Hamza Syed <syed_hamza@msn.com>
2013-05-12 13:39:40 +0200

    jai pas mis de commentaire a IA justee

--- Projet/ia.c
+++ Projet/ia.c
@@ -1,89 +1,21 @@
 #include "ia.h"
 
-/*int ***coupsPossibles;
-int x = 0;
-
-
-void play(char color,Plateau *p){
-	int a,i,d;
-	extern int x;
-	a = 0;
-	int *pion_random = coord_random_pion(p, color);
-	
-	coupsPossible(p, pion_random, color);
-	
-	
-	
-    for(i = 0; i < x ; i++){
-		
-
-		for(d = 1; d < 3; d++){
-			if(d == 1){
-				
-				
-			}
-			else{
-				
-			}
-		}
-	
-    }
-	
-	
-}
-
-void addToList(int ** coor){
-	extern int x;
-	extern int ***coupsPossibles;
-	if( x == 0){
-		coupsPossibles = malloc(6*sizeof(int));
-	}
-	
-	
-	if(coor[0][0] == 2){
-		coupsPossibles[x] = malloc(sizeof(int)*3);
-		coupsPossibles[x][0] = malloc(sizeof(int)*1);
-		coupsPossibles[x][1] = malloc(sizeof(int)*2);
-		coupsPossibles[x][2] = malloc(sizeof(int)*2);
-		coupsPossibles[x][0] = coor[0];
-		coupsPossibles[x][1] = coor[1];
-		coupsPossibles[x][2] = coor[2];
-	}
-	
-	else{
-		coupsPossibles[x] = malloc(sizeof(int)*4);
-		coupsPossibles[x][0] = malloc(sizeof(int)*1);
-		coupsPossibles[x][1] = malloc(sizeof(int)*2);
-		coupsPossibles[x][2] = malloc(sizeof(int)*2);
-		coupsPossibles[x][3] = malloc(sizeof(int)*2);
-		coupsPossibles[x][0] = coor[0];
-		coupsPossibles[x][1] = coor[1];
-		coupsPossibles[x][2] = coor[2];
-		coupsPossibles[x][3] = coor[3];
-	}
-
-	x++;
-
-
-}*/
-
 	static int *** coupsPossibles;
 	static int tailleDeplacement, tailleListe;
 
 int ** play(char color,Plateau *p){
-	int i;
+	
+	/* Fonction générant un coup aléatoire : il prend un pion de la couleur,
+	recherche les possibilités de mouvement à partir de sa coordonnée,
+	et renvoi une de ces possiilité choisi aléatoirement*/
+	
+	int i,**coorRandom;
 	
 	coupsPossibles = malloc(sizeof(int**)*10);
 	tailleDeplacement=0;
 	tailleListe = 10;
 	IA(p,color);
 	
-	for (i = 0; i < tailleDeplacement; i++){
-		
-		
-		
-	}
-	int **coorRandom;
 	int r = rand()%(tailleDeplacement-0) +0;
 	
     coorRandom = coupsPossibles[r];
@@ -94,6 +26,7 @@ int ** play(char color,Plateau *p){
 }
 
 void IA(Plateau *p, char couleur){
+	
 	int i,j,k;
 
 	for(i = 0; i < 9; i++){
@@ -124,6 +57,9 @@ void IA(Plateau *p, char couleur){
 }
 
 void addToList(int ** coor){
+	
+	/* Ajoute une possibilité de deplacement à la liste coupsPossibles */
+	
 	if (tailleListe==tailleDeplacement){
 		 tailleListe*=2;
 		 
